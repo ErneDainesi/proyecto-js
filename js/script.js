ce50ac15e1b233f658d93ca3dd0abb6ef9f12fa2
ErneDainesi <ernestodainesiperrot@gmail.com>
2021-04-26 21:08:47 -0300

    termino 2da entrega de proyecto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,54 @@
-// Por el momento voy a dejar este array (acotado) hasta que veamos lo de AJAX
-// Igualmente, ya tengo armado un .json con las peliculas
-const PELICULAS = [
-  { titulo: "Star Wars: A New Hope", genero: "Ciencia Ficcion", precio: 100 },
-  { titulo: "Top Gun", genero: "Drama", precio: 100 },
-  { titulo: "Scarface", genero: "Accion", precio: 100 },
-  { titulo: "Back to the Future", genero: "Ciencia Ficcion", precio: 100 },
-];
+class Pelicula {
+  constructor(titulo, genero, precio) {
+    this.titulo = titulo;
+    this.genero = genero;
+    this.precio = precio;
+  }
+
+  peliculaTitulo() {
+    return this.titulo;
+  }
+
+  peliculaGenero() {
+    return this.genero;
+  }
+
+  peliculaPrecio() {
+    return this.precio;
+  }
+}
+
+class Carrito {
+  constructor() {
+    this.enCarrito = [];
+    this.total = 0;
+  }
+
+  sumarACarrito(nombrePelicula) {
+    for (const objPelicula of PELICULAS) {
+      if (
+        nombrePelicula == objPelicula.peliculaTitulo() &&
+        !this.enCarrito.includes(objPelicula)
+      ) {
+        this.enCarrito.push(objPelicula);
+        this.total += objPelicula.peliculaPrecio();
+      } else if (
+        nombrePelicula == objPelicula.peliculaTitulo() &&
+        this.enCarrito.includes(objPelicula)
+      ) {
+        alert("¡No se puede alquilar mas de una vez la misma pelicula!");
+      }
+    }
+  }
+
+  elementosDentroDeCarrito() {
+    return this.enCarrito;
+  }
+
+  totalCarrito() {
+    return this.total;
+  }
+}
 
 // Funcion para poder conseguir el numero de id de tag pasado por parametro
 const getIdNumber = (tagId) => tagId.charAt(tagId.length - 1);
@@ -23,23 +66,16 @@ const recuperarCarritoDeLocalStorage = () => {
   return JSON.parse(localStorage.getItem("carrito"));
 };
 
-class Carrito {
-  constructor() {
-    this.enCarrito = [];
-  }
+// Por el momento voy a dejar este array (acotado) hasta que veamos lo de AJAX
+// Igualmente, ya tengo armado un .json con las peliculas
+const PELICULAS = [
+  new Pelicula("Star Wars: A New Hope", "Ciencia Ficcion", 100),
+  new Pelicula("Top Gun", "Drama", 100),
+  new Pelicula("Scarface", "Accion", 100),
+  new Pelicula("Back to the Future", "Ciencia Ficcion", 100),
+];
 
-  sumarACarrito(pelicula) {
-    if (!this.enCarrito.includes(pelicula)) {
-      this.enCarrito.push(pelicula);
-    } else {
-      alert("¡No puede alquilar mas de una vez la misma pelicula!");
-    }
-  }
-
-  elementosDentroDeCarrito() {
-    return this.enCarrito;
-  }
-}
+var carrito = new Carrito();
 
 // Crea boton para ver contenido dentro del carrito
 function generarBotonCarrito() {
@@ -56,11 +92,15 @@ function crearEventoParaBotonCarrito() {
   document
     .querySelector("#verCarritoBtn")
     .addEventListener("click", function () {
-      alert(recuperarCarritoDeLocalStorage().join(", "));
+      let listado = [];
+      for (pelicula of recuperarCarritoDeLocalStorage()) {
+        listado.push(pelicula.titulo);
+      }
+      alert(
+        `${listado.join(", ")}\nEl total a abonar es $${carrito.totalCarrito()}`
+      );
     });
 }
-
-var carrito = new Carrito();
 
 /* Evento cuando el usuario hace click en uno de los botones para alquilar */
 // Se agrega la pelicula elegida a la lista del carrito
@@ -73,9 +113,8 @@ document
     );
     for (const pelicula of listaPeliculas) {
       if (getIdNumber(e.target.id) == getIdNumber(pelicula.id)) {
-        // Le aplico trim porque me estaba guardando el string con espacios
-        // generador por la extension de prettier en vscode
         carrito.sumarACarrito(pelicula.textContent.trim());
+        break; // hago esto para que una vez que encuentre la pelicula, deje de recorrer la lista. De esta manera, es un poco mas eficiente.
       }
     }
     agregarALocalStorage(carrito);
